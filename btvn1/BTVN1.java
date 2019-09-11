4823a40b5c51c0b602e7d5a59b2c118f2894f527
minhduc <minhducducminh1999@gmail.com>
2019-09-11 20:21:03 +0700

    committed

---
+++ btvn1/BTVN1.java
@@ -0,0 +1,58 @@
+package btvn1;
+import java.lang.reflect.Array;
+import java.util.Scanner;
+import java.lang.Math;
+/**
+ * BTVN1
+ */
+public class BTVN1 {
+  static Scanner scan = new Scanner(System.in);
+  public static void main(String[] args) {
+    // baitap1();
+    // baitap2();
+    // baitap3();
+    baitap4();
+  }
+  static void baitap1(){
+    System.out.println("Hello! I’m Hoang Phan Minh Duc.");
+    System.out.println("I’m 20 years old.");
+    System.out.println("This is my first java program.");
+  }
+  static void baitap2(){
+    String mssv, hoten;
+    int tuoi,namsinh;
+    float dtb;
+    System.out.print("MSSV: ");
+    mssv = scan.nextLine();
+    System.out.print("Ho va ten: ");
+    hoten = scan.nextLine();
+    System.out.print("Tuoi: ");
+    tuoi = scan.nextInt();
+    System.out.print("Nam sinh: ");
+    namsinh = scan.nextInt();
+    System.out.print("dtb");
+    dtb = scan.nextFloat();
+    System.out.printf("%s %s %d %d %f\n",mssv,hoten,tuoi,namsinh,dtb);
+  }
+  static void baitap3(){
+    int[] arr = new int[5];
+    for(int i=0;i<arr.length;i++){
+      System.out.printf("Nhap phan tu thu %d: ",i+1);
+      arr[i] = scan.nextInt();
+    }
+    int max = arr[0];
+    for(int i=1;i<arr.length;i++){
+      if (max < arr[i]) max = arr[i];
+    }
+    System.out.printf("So lon nhat trong mang la: %d\n",max);
+  }
+  static void baitap4(){
+    // int m,n;
+    // System.out.print("Nhap m dong: ");
+    // m = scan.nextInt();
+    // System.out.print("Nhap n cot : ");
+    // n = scan.nextInt();
+    // int[][] matrix = new int[m][n];
+    System.out.printf("%f",Math.random());
+  }
+}
