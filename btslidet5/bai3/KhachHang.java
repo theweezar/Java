8ee1446453141fd10581c89453b312fb2674f22e
minhduc <minhducducminh1999@gmail.com>
2019-10-26 12:41:14 +0700

    committed

---
+++ btslidet5/bai3/KhachHang.java
@@ -0,0 +1,50 @@
+package btslidet5.bai3;
+
+import java.util.Scanner;
+
+/**
+ * KhachHang
+ */
+public class KhachHang {
+
+  private String password;
+  private String ten;
+  private long sodu; // số dư trong tài khoản
+
+  public KhachHang(){
+    Nhap();
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public String getTen() {
+    return ten;
+  }
+
+  public void setTen(String ten) {
+    this.ten = ten;
+  }
+
+  public long getSodu() {
+    return sodu;
+  }
+
+  public void setSodu(long sodu) {
+    this.sodu = sodu;
+  }
+
+  public void Nhap(){
+    Scanner scan = new Scanner(System.in);
+    System.out.print("\nNhap ten: ");
+    setTen(scan.nextLine());
+    System.out.print("Nhap mk : ");
+    setPassword(scan.nextLine());
+    setSodu(0);
+  }
+}
---
+++ btslidet5/bai3/NganHang.java
@@ -0,0 +1,15 @@
+package btslidet5.bai3;
+
+import java.util.HashMap;
+
+/**
+ * NganHang
+ */
+public class NganHang {
+
+  private HashMap<String,KhachHang> dskh;
+  
+  public NganHang(){
+    
+  }
+}
