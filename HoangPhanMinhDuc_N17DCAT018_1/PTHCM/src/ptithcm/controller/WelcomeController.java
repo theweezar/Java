3dec291f7cb76421e53a6ea2ac3c9bc3074abaa2
Duc <minhducducminh1999@gmail.com>
2020-04-15 10:54:48 +0700

    committed

---
+++ HoangPhanMinhDuc_N17DCAT018_1/PTHCM/src/ptithcm/controller/WelcomeController.java
@@ -0,0 +1,30 @@
+package ptithcm.controller;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class WelcomeController {
+	@RequestMapping("welcome")
+	public String welcome(){
+		return "welcome";
+	}
+	@RequestMapping("student/form")
+	public String showForm(){
+		return "student/form";
+	}
+	@RequestMapping("student/savedata")
+	public String saveData(HttpServletRequest req){
+		String name = req.getParameter("name");
+		String mark = req.getParameter("mark");
+		String major = req.getParameter("major");
+		
+		req.setAttribute("name", name);
+		req.setAttribute("mark", mark);
+		req.setAttribute("major", major);
+		
+		return "student/success";
+	}
+}
