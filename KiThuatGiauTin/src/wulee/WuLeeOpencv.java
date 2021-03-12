21973546ae79d1214c7ce72b44e44b16e0f4cb77
minhduc <minhducducminh1999@gmail.com>
2021-03-12 12:13:24 +0700

    committed

--- KiThuatGiauTin/src/wulee/WuLeeOpencv.java
+++ KiThuatGiauTin/src/wulee/WuLeeOpencv.java
@@ -26,8 +26,11 @@ public class WuLeeOpencv {
     
     private Mat coverImage = null;
     private Mat key = null;
-    private String message;
+    private String message = "";
     private int hiddenCount = 0;
+    private String retrieveMessage = "";
+    private int retrieveCount = 0;
+    private int retrieveMax = 20;
     private int CHANNEL_BLUE = 0;
     private int CHANNEL_GREEN = 1;
     private int CHANNEL_RED = 2;
@@ -122,35 +125,32 @@ public class WuLeeOpencv {
     
     public Mat hideInBlock(Mat block,  char c, int channel){
         // block ở trên có 3 channel BGR
-        // channel_block là ma trận 8x8 đã được tách từng channel ra theo số int channel
-        Mat channel_block = new Mat(8,8,CvType.CV_8UC1, new Scalar(0));
         List<Mat> listChannel = new ArrayList<Mat>();
+        // Core.split là phương thức tách chiều sau của ma trận rồi return vào 1 list<Mat> đã khởi tạo ở trên
         Core.split(block, listChannel);
-        channel_block = listChannel.get(channel);
-        // Duyệt để tách channel
-//        for(int i = 0; i < 8; i++){
-//            for(int j = 0; j < 8; j++){
-//                channel_block.row(i).col(j).setTo(new Scalar(block.get(i, j)[channel]));
-//            }
-//        }
-        System.out.printf("\nchannel_block:\n%s\n",channel_block.dump());
+        // channel_block là ma trận 8x8 được lấy từ listChannel ở trên thôi thứ tự 0,1,2 tương đương với BGR
+        Mat channel_block = listChannel.get(channel);
+        
+//        System.out.printf("\nchannel_block:\n%s\n",channel_block.dump());
         
         // Chuyển kí tự thành dãy nhị phân 8bit
         String binChar = String.format("%8s", Integer.toBinaryString(c)).replaceAll(" ", "0");
-        System.out.printf("\n%c ===> %s\n", c, binChar);
+//        System.out.printf("\n%c ===> %s\n", c, binChar);
         // Duyệt theo từng cột - channel_block.cols()
         for(int i = 0; i < channel_block.cols(); i++){
+            // Fi là ma trận nhị phân (8,8) của từng cột (8,1)
             Mat fi = fi_to_binary(channel_block.col(i));
             // In ra màn hình giá trị fi int cũ
-            System.out.printf("fi[%d] int :\n%s\n",i,channel_block.col(i).dump());
+//            System.out.printf("fi[%d] int :\n%s\n",i,channel_block.col(i).dump());
             // In ra màn hình giá trị fi binary cũ
-            System.out.printf("fi[%d] bin :\n%s\n",i,fi.dump());
-            // Function bitwise_and giữa fi và key, sau đó return kết quả vào biến matAnd
+//            System.out.printf("fi[%d] bin :\n%s\n",i,fi.dump());
+            // Function Core.bitwise_and giữa fi và key, sau đó return kết quả vào biến matAnd
             Mat matAnd = new Mat();
             Core.bitwise_and(fi, key, matAnd);
+            // Function Core.sumElems là phương thức cộng tất cả phần tử trong ma trận lại
             int and_sum = (int)Core.sumElems(matAnd).val[0];
             int key_sum = (int)Core.sumElems(key).val[0];
-            System.out.printf("fi[%d] ===> and_sum = %d ; key_sum = %d\n", i, and_sum, key_sum);
+//            System.out.printf("fi[%d] ===> and_sum = %d ; key_sum = %d\n", i, and_sum, key_sum);
             if (and_sum > 0 && and_sum < key_sum){
                 int bit = binChar.charAt(i) == '1' ? 1:0;
                 if (and_sum % 2 == bit){
@@ -166,22 +166,34 @@ public class WuLeeOpencv {
                     fi = randomBinaryReverse(fi);
                 }
                 // In ra màn hình giá trị fi binary mới
-                System.out.printf("fi[%d] bin :\n%s\n",i,fi.dump());
+//                System.out.printf("fi[%d] bin :\n%s\n",i,fi.dump());
                 fi = fi_to_int(fi);
                 // In ra màn hình giá trị fi int mới
-                System.out.printf("fi[%d] int :\n%s\n",i,fi.dump());
+//                System.out.printf("fi[%d] int :\n%s\n",i,fi.dump());
                 // Gán giá trị fi mới vào channel_block. Duyệt theo channel_block.rows()
                 for(int j = 0; j < channel_block.rows(); j++){
                     channel_block.put(j, i, fi.get(j, 0));
                 }
-                System.out.printf("\nNewchannel_block:\n%s\n",channel_block.dump());
-                // Khúc này chưa thử, mai thử
-//                Core.merge(listChannel, fi);
+//                System.out.printf("\nNewchannel_block:\n%s\n",channel_block.dump());
             }
         }
-        
+        // Sau khi channel_block đã được chỉnh sửa, ta gán ngược lại vào listChannel ở trên đúng với thứ tự channel
+        listChannel.set(channel, channel_block);
+        // Sau đó merge listChannel mới vào Mat block để ra 1 block ảnh mới đã được giấu tin và return nó về
+        Core.merge(listChannel, block);
+//        System.out.println(block.dump());
+        // Sau khi dấu xong thì hiddenCount sẽ tăng lên 1 vì đã dấu được 1 ký tự
         hiddenCount++;
         return block;
+    }
+    
+    public void assignNewBlockToCoverImage(Mat block, int row, int col){
+        // Đây là phương thức gán block(BGR) vào ảnh(BGR)
+        for(int i = 0; i < 8; i++){
+            for(int j = 0; j < 8; j++){
+                coverImage.put(row + i, col + j, block.get(i, j));
+            }
+        }
     }
     
     public void hideInBlueChannel(){
@@ -193,6 +205,7 @@ public class WuLeeOpencv {
                 if (j + 8 > cols || hiddenCount == message.length()) break;
                 Mat block = coverImage.colRange(j,j+8).rowRange(i,i+8);
                 block = hideInBlock(block, message.charAt(hiddenCount),CHANNEL_BLUE);
+                assignNewBlockToCoverImage(block, i, j);
             }
         }
     }
@@ -222,7 +235,80 @@ public class WuLeeOpencv {
         }
     }
     
+    public void retrieveInBlock(Mat block, int channel){
+        // block ở trên có 3 channel BGR
+        List<Mat> listChannel = new ArrayList<Mat>();
+        // Core.split là phương thức tách chiều sau của ma trận rồi return vào 1 list<Mat> đã khởi tạo ở trên
+        Core.split(block, listChannel);
+        // channel_block là ma trận 8x8 được lấy từ listChannel ở trên thôi thứ tự 0,1,2 tương đương với BGR
+        Mat channel_block = listChannel.get(channel);
+//        System.out.printf("\nchannel_block:\n%s\n",channel_block.dump());
+        
+        String binString = "";
+        for(int i = 0; i < channel_block.cols(); i++){
+            // Fi là ma trận nhị phân (8,8) của từng cột (8,1)
+            Mat fi = fi_to_binary(channel_block.col(i));
+            // In ra màn hình giá trị fi int cũ
+//            System.out.printf("fi[%d] int :\n%s\n",i,channel_block.col(i).dump());
+            // In ra màn hình giá trị fi binary cũ
+//            System.out.printf("fi[%d] bin :\n%s\n",i,fi.dump());
+            // Function Core.bitwise_and giữa fi và key, sau đó return kết quả vào biến matAnd
+            Mat matAnd = new Mat();
+            Core.bitwise_and(fi, key, matAnd);
+            // Function Core.sumElems là phương thức cộng tất cả phần tử trong ma trận lại
+            int and_sum = (int)Core.sumElems(matAnd).val[0];
+            int key_sum = (int)Core.sumElems(key).val[0];
+//            System.out.printf("fi[%d] ===> and_sum = %d ; key_sum = %d\n", i, and_sum, key_sum);
+            if (and_sum > 0 && and_sum < key_sum){
+                if (and_sum % 2 == 0){
+                    binString += "0";
+                }
+                else {
+                    binString += "1";
+                }
+            }
+        }
+        retrieveMessage += String.format("%c", (char)Integer.parseInt(binString, 2));
+        retrieveCount++;
+    }
     
+    public void retrieveInBlueChannel(){
+        int cols = coverImage.cols();
+        int rows = coverImage.rows();
+        for(int i = 0; i < rows; i += 8){
+            if (i + 8 > rows || retrieveCount == retrieveMax) return;
+            for(int j = 0; j < cols; j += 8){
+                if (j + 8 > cols || retrieveCount == retrieveMax) break;
+                Mat block = coverImage.colRange(j,j+8).rowRange(i,i+8);
+                retrieveInBlock(block, CHANNEL_BLUE);
+            }
+        }
+    }
+    
+    public void retrieveInGreenChannel(){
+        
+    }
+    
+    public void retrieveInRedChannel(){
+        
+    }
+    
+    public void retrieve(){
+        // Kiểm lỗi
+        if (coverImage == null){
+            System.out.println("The image has not been imported");
+        }
+        else if (key == null){
+            System.out.println("The key has not been generated");
+        }
+        else{
+            System.out.printf("Begin to retrieve. Rows: %d, Cols: %d\n",coverImage.rows(),coverImage.cols());
+            retrieveInBlueChannel();
+            retrieveInGreenChannel();
+            retrieveInRedChannel();
+            System.out.printf("Retrieve successfully.\nThe hidden messages is: %s\n", retrieveMessage);
+        }
+    }
     
     
     public static void main(String[] args) {
@@ -231,9 +317,9 @@ public class WuLeeOpencv {
         WuLeeOpencv wulee = new WuLeeOpencv();
         wulee.setCoverImage("cat.jpeg");
         wulee.setKey("minhduc");
-        wulee.setMessage("d");
+        wulee.setMessage("ducdeptrai");
         wulee.hide();
-        
+        wulee.retrieve();
     }
 }
 
